c6fd258268b7432818d6a632af2632f47cb83342
sundongmin <sundongmin@unipus.cn>
2019-03-15 17:12:36 +0800

    CountDemo

---
+++ src/main/java/com/github/stream/CountDemo.java
@@ -0,0 +1,13 @@
+package com.github.stream;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class CountDemo {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("AA", "AB", "CC");
+        long count = list.stream().filter(e -> e.startsWith("A")).count();
+        System.out.println(count);
+    }
+}
