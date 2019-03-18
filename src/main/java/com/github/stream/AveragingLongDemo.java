efbdd30055d5234de2afe0455fa175794f7191a0
sundongmin <sundongmin@unipus.cn>
2019-03-18 11:27:19 +0800

    AveragingLongDemo

---
+++ src/main/java/com/github/stream/AveragingLongDemo.java
@@ -0,0 +1,13 @@
+package com.github.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AveragingLongDemo {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        Double result = list.stream().collect(Collectors.averagingLong(x -> x * 2));
+        System.out.println(result);
+    }
+}
