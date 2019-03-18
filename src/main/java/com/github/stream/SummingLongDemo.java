1c7e5da87717c79f8f22d871abbf7cc802daa370
sundongmin <sundongmin@unipus.cn>
2019-03-18 15:50:24 +0800

    SummingLongDemo

---
+++ src/main/java/com/github/stream/SummingLongDemo.java
@@ -0,0 +1,14 @@
+package com.github.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SummingLongDemo {
+    public static void main(String[] args) {
+        List<Long> list = Arrays.asList(340L, 240L, 360L);
+        Long result = list.stream().collect(Collectors.summingLong(i -> i));
+        System.out.println(result);
+    }
+}
