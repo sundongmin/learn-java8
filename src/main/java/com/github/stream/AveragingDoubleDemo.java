695ee50701ec8b12f44bc9f2e917ae19af5dafa9
sundongmin <sundongmin@unipus.cn>
2019-03-18 11:23:22 +0800

    AveragingDoubleDemo

--- src/main/java/com/github/stream/AveragingDoubleDemo.java
+++ src/main/java/com/github/stream/AveragingDoubleDemo.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class AveragingDoubleDemo {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        List<Double> list = Arrays.asList(1.1, 2.2, 3.3, 4.4);
         Double result = list.stream().collect(Collectors.averagingDouble(x -> x * 2));
         System.out.println(result);
     }
