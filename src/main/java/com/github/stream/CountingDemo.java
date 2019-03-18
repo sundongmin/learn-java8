f36a238c30cbcac9f196e9512610b328e012e8a3
sundongmin <sundongmin@unipus.cn>
2019-03-18 15:28:58 +0800

    CountingDemo

---
+++ src/main/java/com/github/stream/CountingDemo.java
@@ -0,0 +1,13 @@
+package com.github.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CountingDemo {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        Long result = list.stream().collect(Collectors.counting());
+        System.out.println(result);
+    }
+}
